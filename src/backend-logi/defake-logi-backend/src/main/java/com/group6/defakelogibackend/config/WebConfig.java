81ecef759c70f59275a805e68004c89822fcb810
何牧 <21101036@buaa.edu.cn>
2025-04-08 13:32:26 +0800

    bug fix

--- src/backend-logi/defake-logi-backend/src/main/java/com/group6/defakelogibackend/config/WebConfig.java
+++ src/backend-logi/defake-logi-backend/src/main/java/com/group6/defakelogibackend/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 对所有路径生效
-                .allowedOrigins("*") // 允许的域名
+                .allowedOriginPatterns("*") // 允许的域名
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 允许的 HTTP 方法
                 .allowedHeaders("*") // 允许的请求头
                 .allowCredentials(true); // 是否允许携带 Cookie
