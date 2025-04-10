0759bc9f9cc65f56dba1d5464b050ab48e74c101
何牧 <21101036@buaa.edu.cn>
2025-04-10 20:35:05 +0800

    添加了作者/论文相关内容

---
+++ src/backend-logi/defake-logi-backend/src/main/java/com/group6/defakelogibackend/model/Author.java
@@ -0,0 +1,18 @@
+package com.group6.defakelogibackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+
+    private long id;
+    private String name;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
--- src/backend-logi/defake-logi-backend/src/test/java/com/group6/defakelogibackend/DefakeLogiBackendApplicationTests.java
+++ src/backend-logi/defake-logi-backend/src/test/java/com/group6/defakelogibackend/DefakeLogiBackendApplicationTests.java
@@ -1,6 +1,8 @@
 package com.group6.defakelogibackend;
 
+import com.group6.defakelogibackend.mapper.AuthorMapper;
 import com.group6.defakelogibackend.mapper.UserMapper;
+import com.group6.defakelogibackend.model.Author;
 import com.group6.defakelogibackend.model.User;
 import com.group6.defakelogibackend.utils.EmailService;
 import com.group6.defakelogibackend.utils.PasswordService;
@@ -9,17 +11,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
+
 @SpringBootTest
 class DefakeLogiBackendApplicationTests {
 
     @Autowired
-    EmailService emailService;
-
-    @Autowired
-    UserMapper userMapper;
+    AuthorMapper authorMapper;
 
     @Test
     void contextLoads() {
+//        List<Author> authors = authorMapper.findAuthor(null, "Shi");
+//        for (Author author : authors) {
+//            System.out.println(author.getName());
+//        }
     }
 
 }
