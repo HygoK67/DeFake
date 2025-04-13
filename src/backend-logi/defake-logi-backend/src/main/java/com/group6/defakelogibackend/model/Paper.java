2dcc3d842d7ac18f97e8de780e7ec62a4b876d48
何牧 <21101036@buaa.edu.cn>
2025-04-13 19:12:13 +0800

    added paper model and mapper

--- src/backend-logi/defake-logi-backend/src/main/java/com/group6/defakelogibackend/model/Paper.java
+++ src/backend-logi/defake-logi-backend/src/main/java/com/group6/defakelogibackend/model/Paper.java
@@ -1,4 +1,26 @@
 package com.group6.defakelogibackend.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paper {
+
+    private Long id;
+    private Long uploaderId;
+    private String title;
+    private String authors;
+    private String abstracT;
+    private String doi;
+    private int pageNum;
+    private String filePath;
+    private LocalDateTime publishedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
